72143958cc89101fbfd4cc8f5ef287b8b2f13dd6
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2025-03-17 17:39:23 -0400

    add position field to solo

--- db/schema/soloEndorsements.ts
+++ db/schema/soloEndorsements.ts
@@ -1,22 +1,29 @@
 import { relations, type InferSelectModel } from 'drizzle-orm';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import { users } from '.';
+import { positions, users } from '.';
 
 export const soloEndorsements = sqliteTable('solo_endorsements', {
-  id: int().primaryKey({ autoIncrement: true }),
-  controllerId: int('controller_id')
-    .notNull()
-    .references(() => users.cid, { onDelete: 'cascade' }),
-  expiresAt: int('expires_at')
-    .notNull()
-    .default(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)
+	id: int().primaryKey({ autoIncrement: true }),
+	controllerId: int('controller_id')
+		.notNull()
+		.references(() => users.cid, { onDelete: 'cascade' }),
+	expiresAt: int('expires_at')
+		.notNull()
+		.default(new Date().getTime() + 30 * 24 * 60 * 60 * 1000),
+	positionId: int('position_id')
+		.notNull()
+		.references(() => positions.id)
 });
 
 export const soloEndorsementRelations = relations(soloEndorsements, ({ one, many }) => ({
-  controller: one(users, {
-    fields: [soloEndorsements.controllerId],
-    references: [users.cid]
-  })
+	controller: one(users, {
+		fields: [soloEndorsements.controllerId],
+		references: [users.cid]
+	}),
+	position: one(positions, {
+		fields: [soloEndorsements.positionId],
+		references: [positions.id]
+	})
 }));
 
 export type SoloEndorsement = InferSelectModel<typeof soloEndorsements>;
